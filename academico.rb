986faa7bf236f654803ad0020d8dfe860c184044
Bernardo B. Marques <bernardo.fire@gmail.com>
2013-02-09 13:36:59 -0200

    implementing grade stuff

--- academico.rb
+++ academico.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 ## Script to print my IFF report card on terminal
 ## TODO: implement features to tell if I passed, etc.
 require "rubygems"
@@ -28,7 +29,25 @@ class Agent
   end
 
   def grade
+    Grade.new(self).printable_grade
+  end
+end
+
+class Grade
+  include Capybara::DSL
+
+  def initialize(agent)
+    @grade_selector =  'table > tbody tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td table > tbody'
+  end
+
+  def go_to_page
     visit '/qacademico/index.asp?t=2032'
+  end
+
+  def extract
+    info = { line_1: ['Matéria', '1ºBi', '2ºBi', 'MS1', '3ºBi', '4ºBi', 'MS2'] }
+    within @grade_selector do
+    end
   end
 end
 
