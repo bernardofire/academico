4687f13b39b912d478fe1516e438ea21e5b0ec3c
Bernardo B. Marques <bernardo.fire@gmail.com>
2013-02-10 03:45:55 -0200

    extract grade info and fix stuff

--- academico.rb
+++ academico.rb
@@ -34,20 +34,37 @@ class Agent
 end
 
 class Grade
-  include Capybara::DSL
-
   def initialize(agent)
-    @grade_selector =  'table > tbody tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td table > tbody'
+    @agent = agent
+    @grade_selector =  'table > tbody tr:nth-child(2) > td > table > tbody > tr.conteudoTexto'
+  end
+
+  def printable_grade
+    go_to_page
+    create_table extract
+  end
+
+  def create_table(info)
+    info #TODO: Implement this method
   end
 
   def go_to_page
-    visit '/qacademico/index.asp?t=2032'
+    @agent.visit '/qacademico/index.asp?t=2032'
   end
 
   def extract
-    info = { line_1: ['Matéria', '1ºBi', '2ºBi', 'MS1', '3ºBi', '4ºBi', 'MS2'] }
-    within @grade_selector do
+    info = { 'line_0' => ['Matéria', '1ºBi', '2ºBi', '3ºBi', '4ºBi'] }
+    line = 1
+    @agent.all(@grade_selector).each do |tr|
+      all_tds = tr.all('td')
+      line_info = []
+      [0, 5, 7, 13, 15] .each do |field|
+         line_info << all_tds[field].text
+      end
+      info["line_#{line}"] = line_info
+      line = line.next
     end
+    info
   end
 end
 
